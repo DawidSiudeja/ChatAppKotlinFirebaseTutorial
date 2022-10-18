f948a97759dcb930312dcb2fe3fae3f44e5d5c45
Dawid Siudeja <dawid.arsyl@gmail.com>
2022-10-18 16:26:46 +0200

    First commit

---
+++ app/src/main/java/com/example/chatapp/SetupProfileActivity.kt
@@ -0,0 +1,78 @@
+package com.example.chatapp
+
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatapp.databinding.ActivitySetupProfileBinding
+import com.example.chatapp.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+
+class SetupProfileActivity : AppCompatActivity() {
+
+    var binding:ActivitySetupProfileBinding? = null
+    var auth:FirebaseAuth? = null
+    var database:FirebaseDatabase? = null
+    var storage:FirebaseStorage? = null
+    var selectedImage: Uri? = null
+    var dialog:ProgressDialog? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySetupProfileBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        dialog!!.setMessage("Updating Profile...")
+        dialog!!.setCancelable(false)
+        database = FirebaseDatabase.getInstance()
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        supportActionBar?.hide()
+        binding!!.imageView.setOnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "image/*"
+            startActivityForResult(intent, 45)
+        }
+        binding!!.setupBtn.setOnClickListener {
+            val name: String = binding!!.profileBox.text.toString()
+            if(name.isEmpty()) {
+                binding!!.profileBox.setError("Please type a name")
+            }
+            dialog!!.show()
+            if (selectedImage != null) {
+                val reference = storage!!.reference.child("Profile")
+                    .child(auth!!.uid!!)
+                reference.putFile(selectedImage!!).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        reference.downloadUrl.addOnCompleteListener { uri ->
+                            val imageUrl = uri.toString()
+                            val uid = auth!!.uid
+                            val phone = auth!!.currentUser!!.phoneNumber
+                            val name: String = binding!!.profileBox.text.toString()
+                            val user = User(uid, name, phone, imageUrl)
+                            database!!.reference
+                                .child("users")
+                                .child(uid!!)
+                                .setValue(user)
+                                .addOnCompleteListener {
+                                    dialog!!.dismiss()
+                                        val intent = Intent(this@SetupProfileActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                        }
+                    }
+                    else {
+                        val uid = auth!!.uid
+                        val phone = auth!!.currentUser!!.phoneNumber
+                        val name: String = binding!!.profileBox.text.toString()
+                    }
+                }
+            }
+        }
+    }
+}
